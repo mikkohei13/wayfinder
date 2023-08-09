b6cc89707d18c7ded4b4881f196c068ed41f9bab
Mikko Heikkinen <hawk@biomi.org>
2023-08-10 00:12:18 +0300

    Second version with frontend polling - works

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask, send_from_directory, jsonify, render_template
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-import time
-import shutil
+from flask import Flask, render_template, send_from_directory, jsonify
 import os
 import json
-
 
 # Load the configuration
 with open('secrets.json', 'r') as file:
@@ -15,33 +10,18 @@ app = Flask(__name__)
 local_dir = 'local_files'
 monitor_dir = config['monitor_dir']
 
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-
-class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        for filename in os.listdir(monitor_dir):
-            if filename not in os.listdir(local_dir):
-                shutil.copy(os.path.join(monitor_dir, filename), os.path.join(local_dir, filename))
-                print(f'Copied: {filename}')
-
-event_handler = MyHandler()
-observer = Observer()
-observer.schedule(event_handler, path=monitor_dir, recursive=False)
-observer.start()
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/get_files')
 def get_files():
-    files = os.listdir(local_dir)
+    files = [f for f in os.listdir(monitor_dir) if os.path.isfile(os.path.join(monitor_dir, f))]
     return jsonify(files)
 
 @app.route('/files/<path:filename>')
 def download_file(filename):
-    return send_from_directory(local_dir, filename)
+    return send_from_directory(monitor_dir, filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
